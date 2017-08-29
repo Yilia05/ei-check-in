54e93c82e03304f160eda17234e0170476ed6626
Hang-Hu <huhang600@gmail.com>
2017-08-29 20:44:57 +0800

    adds secret.json and command-line support

--- README.md
+++ README.md
@@ -1,20 +1,47 @@
 # ci-check-in
 
-Automatic check in tool for eBay Shanghai interns.
+Automatic check in tool for eBay Shanghai interns, written in python 3.
 
 ## Installation
 
 1. Install [Selenium](https://pypi.python.org/pypi/selenium) 
 
 ```
-$ pip install selenium
+pip3 install selenium
 ```
 
 2. Dowload [Chrome driver](https://sites.google.com/a/chromium.org/chromedriver/)
 
+3. Fill in your username and password.
+
+```
+cp secret.json.template secret.json
+```
+
+Then fill in your username and password in `secret.json`.
+
 ## Usage
 
-Set `startdate = "2017-08-21"` and `length = 3` in the code and it will call chrome to check in at 21, 22, 23 for you. If any day in these three days is not weekday, the tool will skip but that weekend will also count for one day in `length.
 Note that the check in will check both morning and afternoon.
+
+This will check in for today.
+
+```
+python3 check-in.py today
+```
+
+This will check-in for August 29, 30, 31 in 2017, and if any day in these three days is not a weekday(Monday to Friday), the tool will skip that day.
+
+```
+python3 check-in.py mul -d 2017-08-29 -l 3
+```
+
+Get help.
+
+```
+python3 check-in.py -h
+python3 check-in.py mul -h
+python3 check-in.py today -h
+```
 
 If you have any improvements, welcome to fork, edits and pull requests.
--- src/check-in.py
+++ src/check-in.py
@@ -2,26 +2,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchWindowException
-import time
 from selenium.webdriver.support.ui import Select
 import datetime
-import sys
 import logging
 import os
-
+import json
+import argparse
 '''
 Author: Hang Hu
 Github: https://github.com/Hang-Hu
 Welcome to join and leave you information here
 '''
 
+def parArg():
+    parser = argparse.ArgumentParser(
+        description='Auto check in for eBay intern in Shanghai.')
+    parser.add_argument('-v', action='version', version='%(prog)s 1.0')
+    subparsers = parser.add_subparsers(help='sub-command help', dest='command')
+    mul_parser = subparsers.add_parser('mul', help='Check in for multiple days.')
+    mul_parser.add_argument('-d', action='store', dest='startdate',
+                        help='The first day to be checked in, format is YYYY-mm-dd such as 2017-08-29.',
+                        required=True)
+    mul_parser.add_argument('-l', action='store', dest='length',
+                        help='The number of days to be checked in.',
+                        required=True)
+    # mul_parser.set_defaults(which='mul')
+    '''action='version' is required for version function'''
+    today_parser = subparsers.add_parser('today', help='Check in for today.')
+    # mul_parser.set_defaults(which='today')
+    args = parser.parse_args()
+    if args.command == 'mul':
+        apply_more(args.startdate, int(args.length))
+    elif args.command == 'today':
+        apply_more(datetime.datetime.today().strftime('%Y-%m-%d'), 1)
+
+
+def readSecret():
+    with open('secret.json') as secret_file:
+        secret = json.load(secret_file)
+
+    return secret["username"], secret["password"]
+
 
 def apply(date, driver):
     driver.get("http://www.ebay-hr-system.com/eleave/")
-
+    username, password = readSecret()
     # username and password for login
-    driver.find_element_by_id("userid").send_keys("I2430")
-    driver.find_element_by_id("pwd").send_keys("hu87...hu")
+    driver.find_element_by_id("userid").send_keys(username)
+    driver.find_element_by_id("pwd").send_keys(password)
     driver.find_element_by_css_selector(".buttonstyle").click()
 
     # hover dropdown menu: my application
@@ -32,11 +60,9 @@ def apply(date, driver):
     # click one link in the dropdown menu
     driver.find_element_by_id("menu_11001170").click()
 
-    print("siwtch to frame")
     # switch to KFrame
     iframe = driver.find_element_by_id('KFrame')
     driver.switch_to_frame(iframe)
-    print("finish switching to frame")
     # hover to another place so that last hover will be canceled
     element_to_hover = driver.find_element_by_xpath(
         '//*[@id="form1"]/div[2]/div/div[2]/div[3]')
@@ -63,7 +89,6 @@ def apply(date, driver):
     # submit application
     driver.find_element_by_xpath(
         '//*[@id="funcPanel"]/div[1]/div/div[2]/a').click()
-    print("accept alert.")
     alert = driver.switch_to_alert()
     alert.accept()
     '''
@@ -74,55 +99,59 @@ def apply(date, driver):
     """
     driver.execute_script("%s" % js)
     '''
-    print("finish accepting alert.")
     # driver.close()
     if "不能重复提交" in driver.page_source:
-        return False
+        return 'has already been checked in, can not be checked in twice.'
     else:
-        return True
+        return 'is checked in successfully.'
 
 
 def openbrowser():
-    #driver = webdriver.PhantomJS()
-    driver=webdriver.Chrome()
+    # driver = webdriver.PhantomJS()
+    driver = webdriver.Chrome()
     driver.set_window_size(1120, 550)
     return driver
 
-os.environ['NO_PROXY'] = '127.0.0.1'
-logging.basicConfig(filename='check-in.log', level=logging.INFO,
-                    format='%(asctime)s %(message)s',
-                    datefmt='%m %d, %Y %I:%M:%S %p')
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
-logger.addHandler(console)
-logger.info("Logs supported.")
-# set encoding=utf8
-reload(sys)
-sys.setdefaultencoding('utf8')
-# set date you want to apply
-startdate = "2017-08-21"  # inclusive
-length = 3  # number of days to be checked in
-dateformate = "%Y-%m-%d"
-startdate = datetime.datetime.strptime(startdate, dateformate).date()
-driver = openbrowser()
 
-for i in xrange(0, length, 1):
-    date = startdate + datetime.timedelta(days=i)
-    if date.isoweekday() in range(1, 6):
-        date = date.strftime(dateformate)
-        while True:
-            try:
-                result = apply(date, driver)
-            except NoSuchWindowException as e:
-                driver = openbrowser()
-            except Exception as e:
-                logger.error("Exception occurs: " + str(e.__class__) + str(e))
-                driver.save_screenshot('exception.png')
-            else:
-                logger.info(date + " " + str(result))
-                break
-    else:
-        logger.info(str(date) + " " + str(date.isoweekday()) +
-                    " is not a weekday")
-driver.close()
+def apply_more(startdate, length):
+    os.environ['NO_PROXY'] = '127.0.0.1'
+    logging.basicConfig(filename='check-in.log', level=logging.INFO,
+                        format='%(asctime)s %(message)s',
+                        datefmt='%m %d, %Y %I:%M:%S %p')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(console)
+    # set encoding=utf8
+    # reload(sys)
+    # sys.setdefaultencoding('utf8')
+    # set date you want to apply
+    # startdate, length = parArg()
+    # startdate = "2017-08-29"  # inclusive
+    # length = 1  # number of days to be checked in
+    dateformate = "%Y-%m-%d"
+    startdate = datetime.datetime.strptime(startdate, dateformate).date()
+    driver = openbrowser()
+
+    for i in range(0, length, 1):
+        date = startdate + datetime.timedelta(days=i)
+        if date.isoweekday() in range(1, 6):
+            date = date.strftime(dateformate)
+            while True:
+                try:
+                    result = apply(date, driver)
+                except NoSuchWindowException as e:
+                    driver = openbrowser()
+                except Exception as e:
+                    logger.error("Exception occurs: " + str(e.__class__) + str(e))
+                    driver.save_screenshot('exception.png')
+                else:
+                    logger.info(date + " " + str(result))
+                    break
+        else:
+            logger.info(str(date) + " " + str(date.isoweekday()) +
+                        " is not a weekday")
+    driver.close()
+
+
+parArg()
